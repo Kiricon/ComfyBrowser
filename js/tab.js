e3f90792c9904d42cf9a5dc0e366432ecff1d0db
kiricon <kiricon@live.com>
2016-10-15 01:26:46 -0600

    Added favicons to the tab :penguin:

--- js/tab.js
+++ js/tab.js
@@ -8,11 +8,18 @@ class Tab {
 
     this.tab = document.createElement('div');
     this.tab.className = "tab";
+    this.tab.innerHTML = '<img class="favicon" src="" /><span class="title"></span>';
     document.getElementById("tabs").appendChild(this.tab);
 
     this.view.addEventListener('did-finish-load', (e) =>{
-      this.tab.innerHTML = this.view.getTitle();
+      this.tab.querySelector(".title").innerHTML = this.view.getTitle();
     });
+
+    this.view.addEventListener('page-favicon-updated', (e)=>{
+      console.log(e);
+      this.tab.querySelector(".favicon").src = e.favicons[0];
+    });
+
     this.listen();
   }
 
