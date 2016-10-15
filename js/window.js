84b3d4c09fbfe7e32153ef1644e3c336bc96fe46
kiricon <kiricon@live.com>
2016-10-14 22:46:19 -0600

    Url bar now works. Just need to parse input.

--- js/window.js
+++ js/window.js
@@ -21,10 +21,32 @@ document.getElementById("min-btn").addEventListener("click", function (e) {
 
 
 //####### Listen for search combo
+var searchBar = document.getElementById('searchBar');
+var webview = document.getElementById('webView');
 
 document.addEventListener("keydown", function(e) {
      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey))      {
        e.preventDefault();
-       alert('yes');
+
+       searchBar.style.display = "block";
+       searchBar.focus();
+
      }
    }, false);
+
+
+searchBar.addEventListener('keypress', function(e){
+  var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      webView.src = searchBar.value;
+      searchBar.value = "";
+      searchBar.style.display = "none";
+    }
+});
+
+//Returns properly formatted url
+function url(input){
+  var url = input;
+
+  return url;
+}
